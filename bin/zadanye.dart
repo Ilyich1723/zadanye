2bda500244b4aaef230569dab322cea49aa90e61
Никита <ilyinnikita90@mail.ru>
2023-10-05 10:37:11 +0300

    first

---
+++ bin/zadanye.dart
@@ -0,0 +1,5 @@
+import 'package:zadanye/zadanye.dart' as zadanye;
+
+void main(List<String> arguments) {
+  print('Hello world: ${zadanye.calculate()}!');
+}
---
+++ test/zadanye_test.dart
@@ -0,0 +1,8 @@
+import 'package:zadanye/zadanye.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
